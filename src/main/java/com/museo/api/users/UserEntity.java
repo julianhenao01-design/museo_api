b074d4778afab2e689b565afade2b0a0597b3ec8
julianhenao01-design <julian.henao01@unicatolica.edu.co>
2026-04-16 15:40:45 -0500

    correccion

--- src/main/java/com/museo/api/users/UserEntity.java
+++ src/main/java/com/museo/api/users/UserEntity.java
@@ -4,18 +4,20 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "users", schema = "users_schema") // Esto lo manda a tu esquema de Railway
+@Table(name = "users", schema = "users_schema")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserEntity {
 
+    @Id // <-- ESTO ES LO QUE FALTA
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // <-- Y ESTO TAMBIÉN
+    private Long id;
+
     @Column(nullable = false, length = 50)
     private String name;
 
     @Column(nullable = false, unique = true, length = 100)
     private String email;
-
 }
-
