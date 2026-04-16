a7528c1dc02573ef41c5c51529d9b2dbd0e5da02
julianhenao01-design <julian.henao01@unicatolica.edu.co>
2026-04-16 15:34:15 -0500

    arregalndo los datos que se debe de pder ¿al usuario

--- src/main/java/com/museo/api/users/UserEntity.java
+++ src/main/java/com/museo/api/users/UserEntity.java
@@ -11,21 +11,11 @@ import lombok.*;
 @AllArgsConstructor
 public class UserEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     @Column(nullable = false, length = 50)
     private String name;
 
     @Column(nullable = false, unique = true, length = 100)
     private String email;
-
-    @Column(nullable = false)
-    private String password;
-
-    @Column(nullable = false)
-    private String role; // Ejemplo: ADMIN, GUIDE, VISITOR
 
 }
 
